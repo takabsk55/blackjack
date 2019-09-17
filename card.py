ba9fb4dffb9b06ec7ce4a6851d86df8fbfc485d5
takahiro <takabsk55@yahoo.co.jp>
2019-09-17 22:27:23 +0900

    完成

--- card.py
+++ card.py
@@ -1,5 +1,26 @@
 import random
 
+class Field():
+    def __init__(self):
+        marks = ["spade", "heart", "clover", "diamond"]*6
+        cards = []
+        for i in marks:
+            for j in range(1, 14):
+                cards.append(Card(i, j))
+
+        self.deck = Deck(cards)
+        self.semetary = Semetary()
+
+
+    def checkdeck(self):
+        if self.deck.decknum()<=0:
+            self.deck=Deck(self.semetary.getsemetary())
+            self.deck.shuffle()
+
+    def draw(self):
+        self.checkdeck()
+        return self.deck.draw()
+
 class Card():
     def __init__(self,mark,num):
         self.num=num
@@ -26,5 +47,5 @@ class Semetary():
         for i in range(len(card)):
             self.semetary.append(card[i])
 
-    def reset(self):
-        return Deck(self.semetary)
+    def getsemetary(self):
+        return self.semetary
--- main.py
+++ main.py
@@ -1,27 +1,110 @@
-from card import Card,Deck,Semetary
+from card import Card,Deck,Semetary,Field
+from player import Player,Dealer
+
+import matplotlib
+matplotlib.use("Agg")
+
+import matplotlib.pyplot as plt
+
+def judge(playerhand,dealerhand):
+    if playerhand > 21:
+        winplayer="dealer"
+        return winplayer
+    if dealerhand > 21:
+        winplayer = "player"
+        return winplayer
+    elif dealerhand > playerhand:
+        winplayer = "dealer"
+        return winplayer
+    elif dealerhand < playerhand:
+        winplayer = "player"
+        return winplayer
+    elif dealerhand == playerhand:
+        winplayer = "draw"
+        return winplayer
+
+def liq(winplayer,bet):
+    if winplayer=="dealer":
+        return 0
+    if winplayer=="player":
+        return bet*2
+    if winplayer=="bj":
+        return bet+bet*1.2
+    if winplayer=="draw":
+        return bet
+
 
 def setup():
-    print("hoo")
-    marks=["spade","heart","clover","diamond"]
-    cards=[]
-    for i in marks:
-        for j in range(1,14):
-            cards.append(Card(i,j))
-    deck=Deck(cards)
 
-    semetary=Semetary()
+    field=Field()
 
-    deck.shuffle()
-    print(deck.decknum())
-    print(deck.draw().num)
-    print(deck.decknum())
+    player=Player(1000)
+
+    dealer=Dealer()
 
 
-    return deck,semetary
+    return field,player,dealer
 
-def main():
-    print("hoge")
+def main(field,player,dealer):
+    wincount=0
+    roop=100000
+    moneyhis=[]
+    for i in range(roop):
+        if player.money<0:
+            print("burst")
+            break
+        winplayer=""
+        #bet=player.pare_bet()
+        #bet=player.marchin_bet()
+        bet=player.bet()
+        dealer.addhand(field.draw())
+
+        player.addhand(field.draw())
+        player.addhand(field.draw())
+
+        while(True):
+            if dealer.checkbj():
+                winplayer="dealer"
+                break
+            if player.checkbj():
+                winplayer="bj"
+                break
+
+            move=player.strategy(dealer.getSumHand())
+
+            if move=="hit":
+                player.addhand(field.draw())
+
+            if move=="double":
+                player.addhand(field.draw())
+                player.money-=bet
+                bet=bet*2
+                move="stand"
+
+            if move=="stand":
+                playerhand=player.getSumHand()
+                while(dealer.getSumHand()<17):
+                    dealer.addhand(field.draw())
+                dealerhand=dealer.getSumHand()
+                winplayer=judge(playerhand,dealerhand)
+                #print(playerhand)
+                break
+        if winplayer=="player" or winplayer=="bj":
+            wincount+=1
+        #print(winplayer)
+        field.semetary.addSemetary(player.trashHand())
+        field.semetary.addSemetary(dealer.trashHand())
+        player.money+=liq(winplayer,bet)
+        moneyhis.append(player.money)
+        player.prebet=bet
+        player.prewin=winplayer
+
+    print(wincount/len(moneyhis))
+    print(player.money)
+    plt.plot(moneyhis)
+    plt.savefig("his")
+
 
 if __name__ == '__main__':
-    setup()
-    main()
+    field,player,dealer=setup()
+    main(field,player,dealer)
